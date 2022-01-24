416d8bf7ab4e96963830b503d5c183e9b8d518be
wahyuts <wahyu.trisna100@gmail.com>
2022-01-24 20:32:04 +0700

    Adding Title

--- components/headnav/index.js
+++ components/headnav/index.js
@@ -1,6 +1,10 @@
 //Ini hanya simple layout silhakan di bikin sendiri headnav nya
 import Image from 'next/image';
+import React, { useState } from 'react';
+import ReactResponsiveHook from '../../utils/ReactResponsiveHook';
+
 import Logo from '../../public/images/Logo.png';
+import MenuIcon from '@mui/icons-material/Menu';
 import styles from '../../styles/Headnav.module.css';
 
 // import { makeStyles } from '@mui/styles';
@@ -15,6 +19,74 @@ import styles from '../../styles/Headnav.module.css';
 
 const Headnav = () => {
     // const classes = useStyles();
+    const [pages, setPages] = useState('Home');
+    const { isMobile, isTablet, isDesktop } = ReactResponsiveHook();
+
+    const handleClickHome = () => {
+        setPages('Home');
+    };
+    const handleClickService = () => {
+        setPages('Services');
+    };
+    const handleClickOurProject = () => {
+        setPages('OurProject');
+    };
+    const handleClickAboutus = () => {
+        setPages('Aboutus');
+    };
+
+    let desktopVersion = (
+        <>
+            <div className={styles.divNav}>
+                {pages === 'Home' ? (
+                    <a style={{ marginRight: 10, color: '#5454D4', cursor: 'pointer' }} onClick={handleClickHome}>
+                        Home
+                    </a>
+                ) : (
+                    <a style={{ marginRight: 10, cursor: 'pointer' }} onClick={handleClickHome}>
+                        Home
+                    </a>
+                )}
+
+                {pages === 'Services' ? (
+                    <a style={{ marginRight: 10, color: '#5454D4', cursor: 'pointer' }} onClick={handleClickService}>
+                        Services
+                    </a>
+                ) : (
+                    <a style={{ marginRight: 10, cursor: 'pointer' }} onClick={handleClickService}>
+                        Services
+                    </a>
+                )}
+
+                {pages === 'OurProject' ? (
+                    <a style={{ marginRight: 10, color: '#5454D4', cursor: 'pointer' }} onClick={handleClickOurProject}>
+                        Our Project
+                    </a>
+                ) : (
+                    <a style={{ marginRight: 10, cursor: 'pointer' }} onClick={handleClickOurProject}>
+                        Our Project
+                    </a>
+                )}
+                {pages === 'Aboutus' ? (
+                    <a style={{ cursor: 'pointer', color: '#5454D4' }} onClick={handleClickAboutus}>
+                        About Us
+                    </a>
+                ) : (
+                    <a style={{ cursor: 'pointer' }} onClick={handleClickAboutus}>
+                        About Us
+                    </a>
+                )}
+            </div>
+            <div className={styles.btnContactUs}>Contact Us</div>
+        </>
+    );
+
+    let mobileVersion = (
+        <div style={{ color: 'white', display: 'flex', alignItems: 'center' }}>
+            <MenuIcon />
+        </div>
+    );
+
     return (
         <div className={styles.fullMainCont}>
             <div className={styles.mainCont}>
@@ -26,13 +98,8 @@ const Headnav = () => {
                         <p className={styles.enverText}>Enver</p>
                     </div>
                 </div>
-                <div className={styles.divNav}>
-                    <a style={{ marginRight: 10, cursor: 'pointer' }}>Home</a>
-                    <a style={{ marginRight: 10, cursor: 'pointer' }}>Services</a>
-                    <a style={{ marginRight: 10, cursor: 'pointer' }}>Our Project</a>
-                    <a style={{ cursor: 'pointer' }}>About Us</a>
-                </div>
-                <div className={styles.btnContactUs}>Contact Us</div>
+
+                {isMobile ? (isTablet ? (isDesktop ? desktopVersion : desktopVersion) : desktopVersion) : mobileVersion}
             </div>
         </div>
     );
